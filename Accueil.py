9499b1d4d3111d34b0cf463b15d1c3cc881f53cb
Abdoulaye NDAO <ndao1301@gmail.com>
2025-11-24 15:21:14 +0000

    Modification

--- Accueil.py
+++ Accueil.py
@@ -31,7 +31,7 @@ f"""
 <style>
 [data-testid="stAppViewContainer"] {{
     background-image: url("data:image/jpg;base64,{arrier_plan}");
-    background-size: 4000px*2251px;
+    background-size: 981px*600px;
 }}
 [data-testid="stSidebar"] > div:first-child {{
     background-image: url("data:image/jpg;base64,{carac}");
